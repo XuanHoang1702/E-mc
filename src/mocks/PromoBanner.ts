1d096c1bf83ab33b4f8d285128d6eafda8446bb6
XuanHoang1702 <tranxuanhoang17022004@gmail.com>
2025-11-09 11:36:58 +0700

    add fake data

--- src/mocks/PromoBanner.ts
+++ src/mocks/PromoBanner.ts
@@ -1,21 +1,57 @@
 const Banner = [
     {
       id: "1",
-      img: "https://picsum.photos/id/1015/600/900?grayscale",
+      img: "https://i.pinimg.com/1200x/fd/00/c1/fd00c1ed31da03fc1a238c9040f2e549.jpg",
       url: "https://example.com/one",
       height: 400,
     },
     {
       id: "2",
-      img: "https://picsum.photos/id/1011/600/750?grayscale",
+      img: "https://i.pinimg.com/736x/ad/43/a2/ad43a2f79fa9b0d936facb972f5c1e83.jpg",
       url: "https://example.com/two",
-      height: 250,
+      height: 450,
     },
     {
       id: "3",
       img: "https://i.pinimg.com/736x/c5/8b/fe/c58bfec265efcd5652ebb7a92a3557d9.jpg",
       url: "https://example.com/three",
       height: 600,
+    },
+    {
+      id: "4",
+      img: "https://i.pinimg.com/736x/5e/96/44/5e96440cd41c00add431ccf2cf1d6443.jpg",
+      url: "https://example.com/three",
+      height: 1200,
+    },
+    {
+      id: "5",
+      img: "https://i.pinimg.com/736x/ad/43/a2/ad43a2f79fa9b0d936facb972f5c1e83.jpg",
+      url: "https://example.com/three",
+      height: 700,
+    },
+    {
+      id: "6",
+      img: "https://i.pinimg.com/736x/32/b7/21/32b721c149844016014ad6b4f5007bf6.jpg",
+      url: "https://example.com/three",
+      height: 700,
+    },
+    {
+      id: "7",
+      img: "https://i.pinimg.com/736x/c9/e0/c4/c9e0c48f71825e2617156c5759114b38.jpg",
+      url: "https://example.com/three",
+      height: 700,
+    },
+    {
+      id: "8",
+      img: "https://i.pinimg.com/1200x/b7/77/21/b777213f65f4312ff34a79d5206d2067.jpg",
+      url: "https://example.com/three",
+      height: 550,
+    },
+    {
+      id: "9",
+      img: "https://i.pinimg.com/736x/1b/0e/05/1b0e05dbb029ef01f6857f65d7972611.jpg",
+      url: "https://example.com/three",
+      height: 480,
     },
 ];
 
