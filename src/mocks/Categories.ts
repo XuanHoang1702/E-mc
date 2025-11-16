5a2f969b78a6a2800a8543b560f854043b9f3d6a
XuanHoang1702 <tranxuanhoang17022004@gmail.com>
2025-11-16 20:02:58 +0700

    no cmt

---
+++ src/mocks/Categories.ts
@@ -0,0 +1,51 @@
+const Categorties = [
+    {
+        image: '/images/ring.jpg',
+        text: 'Ring'
+      },
+      {
+        image: '/images/Earrings.jpg',
+        text: 'Earrings'
+      },
+      {
+        image: '/images/Clock.jpg',
+        text: 'Clock'
+      },
+      {
+        image: '/images/Bracelet.jpg',
+        text: 'Bracelet'
+      },
+      {
+        image: '/images/Necklace.jpg',
+        text: 'Necklace'
+      },
+      {
+        image: 'images/Dress.jpg',
+        text: 'Dress'
+      },
+      {
+        image: 'images/High-heels.jpg',
+        text: 'High heels'
+      },
+      {
+        image: 'images/Bag.jpg',
+        text: 'Bag'
+      },
+      {
+        image: 'images/jacket.jpg',
+        text: 'Jacket'
+      },
+      {
+        image: 'images/Perfume.jpg',
+        text: 'Perfume'
+      },
+      {
+        image: 'images/Hairpin.jpg',
+        text: 'Hairpin'
+      },
+      {
+        image: 'images/Scarf.jpg',
+        text: 'Scarf'
+      }
+];
+export default Categorties;
